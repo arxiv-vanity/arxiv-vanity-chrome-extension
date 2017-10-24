da7dc232a3fc1f7dc5a45a8c000f630c2fc4eda2
isaykatsman <isaykatsman@hotmail.com>
2017-10-24 18:30:43 -0400

    Added necessary functionality, link injection to arxiv page complete

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Chrome-Arxiv-Vanity
-Chrome extension that adds "arxiv-vanity" link to the arxiv download options
+
+An extension that provides an [Arxiv Vanity](https://www.arxiv-vanity.com) link directly on an accessed arxiv page.
+
+# Installation (unofficial)
+
+1. Clone this repo.
+2. Open the [chrome://extensions](chrome://extensions) page.
+3. Check the *Developer Mode* box on the top of the page.
+4. Click "Load unpacked extension..." and select the repo folder.
+
+# Usage
+
+Navigate to any [arxiv.org][arxiv] paper, click the "Arxiv Vanity" link the the downloads section.
---
+++ arxivvanitymod.js
@@ -0,0 +1,20 @@
+var div_dl = document.getElementsByClassName("full-text")[0];
+var c = div_dl.childNodes;
+var i;
+for (i = 0; i < c.length; i++) {
+    if(c[i].nodeName == "UL") {
+        var node = document.createElement("LI");
+        var link = document.createElement("A");
+        var t = document.createTextNode("Arxiv Vanity");
+        // get url, extract arxiv id
+        url = window.location.href
+        re = /(\d+\.\d+v?\d)/i
+        found = url.match(re)[0]
+        av_url = "https://www.arxiv-vanity.com/papers/" + found
+        link.setAttribute("href", av_url);
+        link.appendChild(t);
+        node.appendChild(link);
+        c[i].insertBefore(node, c[i].childNodes[2]);
+        break;
+    }
+}
