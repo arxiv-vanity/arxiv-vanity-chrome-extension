6369afd98f6779ed28390e0a4f78f7dccf42442c
Ben Firshman <ben@firshman.co.uk>
2021-12-27 19:03:11 +0000

    Remove tabs permission

--- background.js
+++ background.js
@@ -1,25 +1,19 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var tab = tabs[0];
+    // extract url and return if doesn't match
+    if(!tab.url.startsWith('https://arxiv.org/') ) {
+        return;
+    }
 
-        // extract url and return if doesn't match
-        url = tab.url;
-        if(!url.startsWith('https://arxiv.org/') ) {
-            return;
-        }
+    // get arxiv id
+    var match = tab.url.match(/(\d+\.\d+v?\d)/i);
+    if (!match) {
+        return;
+    }
 
-        // get arxiv iv
-        re = /(\d+\.\d+v?\d)/i
-        found = url.match(re)[0]
-
-        // construct new url
-        vanity = "http://www.arxiv-vanity.com/papers/" + found
-
-        // navigate to vanity
-        chrome.tabs.create({ 
-            url: vanity,
-            index: tab.index + 1,
-            active: true,
-        });
+    // navigate to vanity
+    chrome.tabs.create({ 
+        url: "https://www.arxiv-vanity.com/papers/" + match[0] + "/",
+        index: tab.index + 1,
+        active: true,
     });
 });
